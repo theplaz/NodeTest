c09c8df4a9833734e0a18130791c64ab181fb689
Michael Plasmeier <plaz@theplaz.com>
2015-07-24 14:04:21 -0700

    add tutorial

---
+++ server.js
@@ -0,0 +1,7 @@
+port = 1337;
+var express = require('./config/express');
+var app = express();
+app.listen(port);
+module.exports = app;
+console.log('Server running at http://localhost:' + port);
+
